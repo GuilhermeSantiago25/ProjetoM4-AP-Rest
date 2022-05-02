a2d252aba7e47222dc5553dcd23cf9d4b265bf6c
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-05-02 09:56:57 -0300

    adicionando configs pro heroku

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { selectAllAlunos, selectIdAlunos, insertAluno, putAlunos, patchAlunos, deleteAluno } from '../controllers/aluno-controller.js';
 
-
 const router = Router();
 
 router.get('/', (req,res)=>{
