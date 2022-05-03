a164132fb5a3be7decc5596c198717eb101545ba
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-05-03 15:06:51 -0300

    corrigindo erros de caminho

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import router from './src/routes/routes.js'
+import router from './routes/routes.js'
 
 const app = express();
 app.use(express.json());
